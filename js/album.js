fdf57ffb1f09e4e94ad2b2fae62955874c3275d6
Rich Ludwig <developer-mac@rmlsn.net>
2015-10-07 14:31:23 -0500

    Overhauled album code, created thumbs, etc.

--- js/album.js
+++ js/album.js
@@ -1,7 +1,6 @@
 // Residential
 var images = {
 	'files' : [
-		{'source' : "033.JPG"},
 		{'source' : "230.JPG"},
 		{'source' : "234.JPG"},
 		{'source' : "236.JPG"},
@@ -13,7 +12,6 @@ var images = {
 		{'source' : "262.JPG"},
 		{'source' : "DSCF0244.JPG"},
 		{'source' : "DSCF0254.JPG"},
-		{'source' : "DSCF0280-1.JPG"},
 		{'source' : "DSCF0280.JPG"},
 		{'source' : "DSCF0285.JPG"},
 		{'source' : "DSCF0302.JPG"},
@@ -24,17 +22,16 @@ var images = {
 		{'source' : "DSC_0031.JPG"},
 		{'source' : "DSC_0043.JPG"},
 		{'source' : "DSC_0045.JPG"},
-		{'source' : "DSC_0151.JPG"},
 		{'source' : "HT logo picture.JPG"},
 		{'source' : "LED House.JPG"},
 		{'source' : "Sandy B Web Ready.jpg"},
-		{'source' : "September Wilson House.jpg"}
+		{'source' : "September Wilson House.jpg"},
+		{'source' : "033.JPG"}
 	]
 }
 // Commercial
 var images_corp = {
 	'files' : [
-		{'source' : "033.JPG"},
 		{'source' : "048.JPG"},
 		{'source' : "DSC00181.JPG"},
 		{'source' : "DSCF0261.JPG"},
@@ -43,25 +40,26 @@ var images_corp = {
 		{'source' : "Eldercare photo.jpg"},
 		{'source' : "Stygar best pic.jpg"},
 		{'source' : "fredweberlogo.jpg"},
-		{'source' : "fredwebertruck.jpg"}
+		{'source' : "fredwebertruck.jpg"},
+		{'source' : "033.JPG"}
 	]
 }
 
 $(document).ready(function(){
-    $("#album-list").append("<div class='item active'><img src='image/res.JPG' alt='' /></div>");
-
 	function buildAlbum(){
+		var first_class = 'active';
 		for(image in images.files) {
-			$("#album-list").append("<div class='item'><img src='image/album/" + images.files[image].source + "' alt='' /></div>");
+			$("#album-list").append("<div class='item " + first_class + "'><a target='_ht_photo' href='image/album/large_" + images.files[image].source + "'><img src='image/album/" + images.files[image].source + "' alt='' /></a></div>");
+			first_class = ' ';
 		}
 	}
 	buildAlbum();
 
-    $("#album-list-corp").append("<div class='item active'><img src='image/com.JPG' alt='' /></div>");
-
 	function buildAlbumCorp(){
+		var first_class = 'active';
 		for(image in images_corp.files) {
-			$("#album-list-corp").append("<div class='item'><img src='image/album_corp/" + images_corp.files[image].source + "' alt='' /></div>");
+			$("#album-list-corp").append("<div class='item " + first_class + "'><a target='_ht_photo' href='image/album_corp/large_" + images_corp.files[image].source + "'><img src='image/album_corp/" + images_corp.files[image].source + "' alt='' /></a></div>");
+			first_class = ' ';
 		}
 	}
 	buildAlbumCorp();
